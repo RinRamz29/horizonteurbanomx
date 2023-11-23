e80e2dd58103153de2cb4d297279f6fdc0fa8118
Rin <rinramz@outlook.com>
2023-11-23 00:01:55 -0600

    Fixed htaccess

--- login.php
+++ login.php
@@ -39,7 +39,7 @@
                     $_SESSION['usuario'] = $usuario['email'];
                     $_SESSION['login'] = true;
 
-                    header('Location: /admin/index.php');
+                    header('Location: /admin/');
 
                 } else {
                     $errores[] = "El password es incorrecto";
